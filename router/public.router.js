256fbf0f86bedfbebd793efb86f9d734385c0d6c
mayuri khade <mayurikhade85@gmail.com>
2024-10-04 13:32:03 +0530

    rate limit

--- router/public.router.js
+++ router/public.router.js
@@ -1,13 +1,16 @@
 const router = require("express").Router()
+const { reteLimit, default: rateLimit } = require("express-rate-limit")
 const public = require("./../controller/public.controller")
 
 router
-    .get("/get-carousel", public.getCarousel)
+
+    .get("/get-carousel", rateLimit({ windowMs: 1 * 60 * 1000, limit: 1 }), public.getCarousel)
     .get("/get-projects", public.getprojects)
     .get("/get-projects-details/:id", public.getProjectDetails)
-    //
 
-    .post("/add-enquiry", public.addEnquiry)
+
+    //enquiey
+    .post("/add-enquiry", rateLimit({ windowMs: 15 * 60 * 1000, limit: 1 }), public.addEnquiry)
     .get("/enquiry", public.getEnquiry)
     .put("/update-enquiry/:id", public.updateEnquiry)
     .delete("/delete-enquiry/:id", public.deleteEnquiry)
