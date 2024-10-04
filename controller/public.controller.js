fc36b4571fba25f69748c353cea057e2b77cd891
mayuri khade <mayurikhade85@gmail.com>
2024-10-04 11:47:55 +0530

    controller

--- controller/public.controller.js
+++ controller/public.controller.js
@@ -3,6 +3,7 @@ const asyncHandler = require("express-async-handler")
 const Projects = require("../model/Projects")
 const Enquiry = require("../model/Enquiry")
 const Carousel = require("../model/Carousel")
+const { checkEmpty } = require("../utils/checkEmpty")
 
 
 exports.getCarousel = asyncHandler(async (req, res) => {
